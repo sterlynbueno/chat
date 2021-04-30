55de235302d2f92899ca0bde502d2972c5306db9
arlinj <arlindejesusmartinez919@gmail.com>
2021-04-29 20:24:05 -1200

    nuevos mensajes

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,3 +1,6 @@
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
 importar  MessageForm  from  './MessageForm' ;
 importar  MyMessage  from  './MyMessage' ;
 importar  TheirMessage  from  './TheirMessage' ;
@@ -14,6 +17,18 @@ const renderMessages = () => {
         const message = messages[key];
         const lastMessageKey = index === 0 ? null : keys[index - 1];
         const isMyMessage = userName === message.sender.username;
+    
+        return (
+            <div key={`msg_${index}`} style={{ width: '100%' }}>
+              <div className="message-block">
+                {
+                    isMyMessage
+                  ? <MyMessage/>
+                  : <TheirMessage/>
+                }
+                
+                 </div>
+                  </div>
     })
 }
 
