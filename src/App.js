e65c607d585e15894f153fd6d02f1f89c8811af9
sterlyn <sterlinbuenosanchez26@gmail.com>
2021-04-30 12:01:57 -0400

    modificacion del chats y reparicion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { ChatEngine } from 'react-chat-engine';
-import ChatFeed from '../../../Downloads/chat_application-main/src/components/ChatFeed';
+
 
 import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
     
-    return (
-        <ChatEngine 
-        height="100vh"
-        projectID="6419cb30-a0fe-4603-988b-8ca547f207ce"
-        userName="javascriptmastery"
-        userSecret="123123"
-        renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps }    />
-        );
-      };
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="6419cb30-a0fe-4603-988b-8ca547f207ce"
+      userName="javascriptmastery"
+      userSecret="123123"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+     
+    />
+  );
+};
+
 
 export default App;
+      
