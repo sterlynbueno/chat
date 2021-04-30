68a5b8c2dc5e1218d9362f0b6f1e0175f386b121
arlinj <arlindejesusmartinez919@gmail.com>
2021-04-29 19:56:57 -1200

    devio de llamada

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -5,7 +5,15 @@ importar  TheirMessage  from  './TheirMessage' ;
 const  ChatFeed  =  ( props )  =>  {
     const  { chats , activeChat , userName , messages }  =  props ;
   
-    const  chat  =  chats  &&  chats [ activeChat ] ;
+    const  chat  =  chats  &&  chats[ activeChat ] ;
+
+const renderMessages = () => {
+    const keys = object.keys(messages);
+
+    return keys.map((key, index) => {
+
+    })
+}
 
 return (
     <div>
