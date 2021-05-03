e4a024276d0be09c59e6cedcd0d7ec6d62891cab
marileidy2829 <marileidycastillo029@gmail.com>
2021-05-03 02:25:40 -0400

    campo usuario y campo contaseña

--- src/components/LoginForma.jsx
+++ src/components/LoginForma.jsx
@@ -1,2 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+}
