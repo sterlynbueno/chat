db5a11b69b1bed9f2d1904e9d574c8d1eb7f855b
marileidy2829 <marileidycastillo029@gmail.com>
2021-05-03 00:59:10 -0400

    modificacion del MessageForm

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,20 +1,46 @@
-const MessageForm = ({ message }) => {
-    if(message?.attachments?.length > 0){
-return (
-    <img
-    src={message.attachments[0].file}
-    alt="message-attachment"
-    className="message-image"
-    style={{float:'right'}}
-    />
-)
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
+const MessageForm = (props) => {
+  const [value, setValue] = useState('');
+  const { chatId, creds } = props;
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
     }
-    return(
-        <div className="message" style={{float: 'right', marginRight: '128px', color: 'white' , backgroundColor: '#3B2A50' }}>
-            {Message.text}
-        </div>
-    );
-}
+
+    setValue('');
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: '' });
+  };
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
+
+      
+      
+    </form>
+  );
+};
 
 export default MessageForm;
