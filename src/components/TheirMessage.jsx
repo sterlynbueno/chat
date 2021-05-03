48fc4aa997b2075e9677c7ccae6180afde3baaf2
marileidy2829 <marileidycastillo029@gmail.com>
2021-05-03 00:30:54 -0400

    Estilos o clases a los mensajers del usuario

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -10,8 +10,22 @@ className="message-avatar"
 style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
 />
             )}
-        </div>
-    );
-}
-
-export default TheirMessage;
+            {message.attachments && message.attachments.length > 0
+              ? (
+                <img
+                  src={message.attachments[0].file}
+                  alt="message-attachment"
+                  className="message-image"
+                  style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                />
+              )
+              : (
+                <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                  {message.text}
+                </div>
+              )}
+          </div>
+        );
+      };
+      
+      export default TheirMessage;
