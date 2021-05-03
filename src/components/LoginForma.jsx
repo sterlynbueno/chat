56ecf8bad284f42849db144f87a0825176da26d3
marileidy2829 <marileidycastillo029@gmail.com>
2021-05-03 02:38:05 -0400

    modificacion y tamaño de letra del formulario

--- src/components/LoginForma.jsx
+++ src/components/LoginForma.jsx
@@ -4,4 +4,19 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleSubmit = () => {
+
+    }
+
+  return(
+      <div className ="wrapper">
+          <div className="form">
+          <h1 className= "title"> chat application </h1>
+          <form onSubmit={handleSubmit}>
+              <imput type="text" value= {username} onChange={} />
+              </form>
+              </div>
+              </div>
+  );  
+
 }
