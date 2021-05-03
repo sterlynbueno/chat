14e312c2d7d02d97ef9953f28cc7ccbfd23dc349
marileidy2829 <marileidycastillo029@gmail.com>
2021-05-03 00:18:43 -0400

    Message-avatar

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,15 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+const isFirstMessaByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+
     return(
-        <div>
-            TheirMessage
+        <div className="message-row">
+            {isFirstMessageByUser && (
+
+<div
+className="message-avatar"
+style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
+/>
+            )}
         </div>
     );
 }
