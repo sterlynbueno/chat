afc02fa6d4d41528860f60f846f5b1f445563332
Edward <edwardanon02612@gmail.com>
2021-05-03 11:44:59 -1200

    Finalizacion del boton

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import axios from 'axios';
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': "6419cb30-a0fe-4603-988b-8ca547f207ce", 'User-Name': username, 'Use-Secret': password };
+
+try {
+
+await axios.get('https://api.chatengine.io/chats', { headers:authObject });
+
+localStorage.setItem('username', username);
+localStorage.setItem('password', password);
+
+
+window.location.reload();
+} catch (error){
+    //error -> try with new username... 
+
+}
+
+
+
+    }
+
+  return(
+      <div className ="wrapper">
+          <div className="form">
+          <h1 className= "title"> chat application </h1>
+          <form onSubmit={handleSubmit}>
+              <imput type="text" value= {username} onChange={(e) => setUsername(e.target.value)} className="imput" placeholder="Username" required />
+              <imput type="password" value= {password} onChange={(e) => setPassword(e.target.value)} className="imput" placeholder="password" required />
+              
+<div align="center">
+    <button type="submit" className="button">
+        <span>Start Chatting</span>
+        </button>
+    </div>
+
+              </form>
+              </div>
+              </div>
+  );  
+
+}
+
+export default LoginForm; // Ojo con el punto y coma
