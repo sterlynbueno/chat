10044e896b3e1162757a09fd0651ce1250a3d8ab
Adonys Santos <adonysmundotech@gmail.com>
2021-05-06 22:32:30 -0400

    refactor: Format component LoginForm.jsx

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,48 +1,68 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+
+const projectID = "c2c0514f-07f1-497c-acc4-a77b945731ad";
+
 const LoginForm = () => {
- const [username, setUsername] = useState('');
- const [password, setPassword] = useState('');
- const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-const handleSubmit = async (e) => {
-   e.preventDefaut();
+  const handleSubmit = async (e) => {
+    e.preventDefaut();
 
-   const authObject = {'Project-ID': "c2c0514f-07f1-497c-acc4-a77b945731ad", 'User-Name': username, 'User-Secret': password};
+    const authObject = {
+      "Project-ID": projectID,
+      "User-Name": username,
+      "User-Secret": password,
+    };
 
-   try { 
-   await axios.get('https://api.chatengine.io/chats', { headers: authObject});
-   
-   localStorage.setItem('username', username);
-   localStorage.setItem('password', password);
+    try {
+      await axios.get("https://api.chatengine.io/chats", {
+        headers: authObject,
+      });
 
-   window.location.reload();
-} catch (error){
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
 
-    setError('Oops, los datos son incorrectos')
-}
-}
- return (
-<div className="wrapper">
-    <div className="form">
+      window.location.reload();
+    } catch (error) {
+      setError("Oops, los datos son incorrectos");
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="form">
         <h1 className="title"> CHAT</h1>
-        <form onSubmit={ handleSubmit}>
-<input type = "text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required/>
-<input type = "text" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required/>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+            placeholder="Password"
+            required
+          />
 
-        <div align="center">
-
-            <button type="submit" className="button" >
-                <span>EMPEZEMOs EL CHAT</span>
+          <div align="center">
+            <button type="submit" className="button">
+              <span>EMPEZEMOS EL CHAT</span>
             </button>
-
-            </div>
-<h2  className="error">{error}</h2>
-              </form>
-              </div>
-              </div>
-  );  
-
-}
+          </div>
+          <h2 className="error">{error}</h2>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default LoginForm;
