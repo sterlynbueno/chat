60af8a6488bbd6f9ca46f781bb56aadbb68ff66b
Adonys Santos <adonysmundotech@gmail.com>
2021-05-06 22:30:44 -0400

    refactor: Format App.js

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./src/components/ChatFeed";
+import LoginForm from "./src/components/LoginForm";
+import "./App.css";
 
-import ChatFeed from './src/components/ChatFeed';
-import LoginForm from './src/components/LoginForm';
-import './App.css';
-
-const projectID = 'c2c0514f-07f1-497c-acc4-a77b945731ad';
+const projectID = "c2c0514f-07f1-497c-acc4-a77b945731ad";
 
 const App = () => {
-  if (!localStorage.getItem('username')) return <LoginForm />;
+  if (!localStorage.getItem("username")) return <LoginForm />;
 
   return (
     <ChatEngine
       height="100vh"
       projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
 
 export default App;
-      
