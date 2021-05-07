7db90722284570d6c2dac969b914e2c1deb207c0
Adonys Santos <adonysmundotech@gmail.com>
2021-05-06 22:31:40 -0400

    refactor: Join App.js files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { ChatEngine } from 'react-chat-engine';
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
 
-
-import LoginForm from './components/LoginForm';
-import ChatFeed from './components/ChatFeed';
-
-
-import './App.css';
+const projectID = "c2c0514f-07f1-497c-acc4-a77b945731ad";
 
 const App = () => {
-  if(!localStorage.getItem('username')) return <LoginForm /> //Return Login form in case that you are not registered
-    
+  if (!localStorage.getItem("username")) return <LoginForm />; //Return Login form in case that you are not registered
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="6419cb30-a0fe-4603-988b-8ca547f207ce"
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-     
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
   );
 };
 
-
 export default App;
-      
